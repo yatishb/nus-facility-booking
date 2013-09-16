c8ec1a239a7ad011c2839157f2ab80c9b02d4622
Shekhar Baggavalli Raju <brshekhar1@gmail.com>
2013-09-16 13:20:07 +0800

    Added SQL schema and project doc

---
+++ ddl_cs2102.sql
@@ -0,0 +1,60 @@
+CREATE TABLE region (
+reg_id CHAR(10) PRIMARY KEY,
+name VARCHAR(256) NOT NULL,
+location VARCHAR(256)
+);
+
+CREATE TABLE user (
+user_id CHAR(8) PRIMARY KEY,
+name VARCHAR(32) NOT NULL,
+password CHAR(32) NOT NULL,
+is_admin BOOLEAN
+);
+
+CREATE TABLE facility (
+fac_id CHAR(10),
+reg_id CHAR(10),
+open_time TIME NOT NULL,
+close_time TIME NOT NULL,
+capacity INT,
+FOREIGN KEY (reg_id) REFERENCES region(reg_id) ON DELETE CASCADE,
+PRIMARY KEY (fac_id,reg_id),
+CHECK(close_time > open_time)
+);
+
+CREATE TABLE booking (
+book_id CHAR(10),
+fac_id CHAR(10),
+reg_id CHAR(10),
+user_id CHAR(10),
+start DATETIME NOT NULL,
+end DATETIME NOT NULL,
+FOREIGN KEY (fac_id) REFERENCES facility(fac_id),
+FOREIGN KEY (reg_id) REFERENCES region(reg_id),
+FOREIGN KEY (user_id) REFERENCES user(user_id),
+PRIMARY KEY (book_id),
+CHECK (end > start)
+);
+
+CREATE TABLE academic (
+fac_id CHAR(10),
+reg_id CHAR(10),
+whiteboard BOOLEAN,
+audio_system BOOLEAN,
+projector BOOLEAN,
+type VARCHAR(100),
+FOREIGN KEY (fac_id) REFERENCES facility(fac_id),
+FOREIGN KEY (reg_id) REFERENCES region(reg_id),
+PRIMARY KEY (fac_id,reg_id)
+);
+
+CREATE TABLE sports (
+fac_id CHAR(10),
+reg_id CHAR(10),
+scoreboard BOOLEAN,
+spectator_area BOOLEAN,
+type VARCHAR(100),
+FOREIGN KEY (fac_id) REFERENCES facility(fac_id),
+FOREIGN KEY (reg_id) REFERENCES region(reg_id),
+PRIMARY KEY (fac_id,reg_id)
+);
