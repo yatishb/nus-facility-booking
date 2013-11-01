61f5a41792d04d944312b183b5e2d72b8d70703d
Anirup <asg.wolfblood@gmail.com>
2013-11-01 23:20:54 +0800

    DB Connection

---
+++ inc/db-conn.php
@@ -0,0 +1,21 @@
+<?php
+function setup_db()
+{
+	$conn=mysql_connect("localhost","root","","cs2102");
+	mysql_select_db("cs2102",$conn);
+	// Check connection
+	if (!$conn)
+	{
+	echo "Failed to connect to MySQL: "; //. mysql_connect_error();
+	}
+	else
+	{
+		return $conn;
+	}
+}
+
+function close_db($conn)
+{
+	mysql_close($conn);
+}	
+?>
