b247c9f5c486afdcaff9fbb30a8c246a7e159265
Yatish <yatishby@gmail.com>
2013-11-14 12:28:39 +0800

    Modified DDL code to match report

--- addl/ddl_cs2102.sql
+++ addl/ddl_cs2102.sql
@@ -16,7 +16,8 @@ fac_id CHAR(10),
 reg_id CHAR(10),
 open_time TIME NOT NULL,
 close_time TIME NOT NULL,
-capacity INT,
+capacity INT NOT NULL,
+name VARCHAR(100) NOT NULL,
 FOREIGN KEY (reg_id) REFERENCES region(reg_id) ON DELETE CASCADE,
 PRIMARY KEY (fac_id,reg_id),
 CHECK(close_time > open_time)
@@ -29,8 +30,8 @@ reg_id CHAR(10),
 user_id CHAR(10),
 start DATETIME NOT NULL,
 end DATETIME NOT NULL,
-FOREIGN KEY (fac_id, reg_id) REFERENCES facility(fac_id, reg_id),
-FOREIGN KEY (user_id) REFERENCES user(user_id),
+FOREIGN KEY (fac_id, reg_id) REFERENCES facility(fac_id, reg_id) ON DELETE SET NULL,
+FOREIGN KEY (user_id) REFERENCES user(user_id) ON DELETE CASCADE,
 PRIMARY KEY (book_id),
 CHECK (end > start)
 );
@@ -38,20 +39,18 @@ CHECK (end > start)
 CREATE TABLE academic (
 fac_id CHAR(10),
 reg_id CHAR(10),
-whiteboard BOOLEAN,
-audio_system BOOLEAN,
-projector BOOLEAN,
-type VARCHAR(100),
-FOREIGN KEY (fac_id, reg_id) REFERENCES facility(fac_id, reg_id),
+whiteboard BOOLEAN DEFAULT FALSE,
+audio_system BOOLEAN DEFAULT FALSE,
+projector BOOLEAN DEFAULT FALSE,
+FOREIGN KEY (fac_id, reg_id) REFERENCES facility(fac_id, reg_id) ON DELETE CASCADE,
 PRIMARY KEY (fac_id,reg_id)
 );
 
 CREATE TABLE sports (
 fac_id CHAR(10),
 reg_id CHAR(10),
-scoreboard BOOLEAN,
-spectator_area BOOLEAN,
-type VARCHAR(100),
-FOREIGN KEY (fac_id, reg_id) REFERENCES facility(fac_id, reg_id),
+scoreboard BOOLEAN DEFAULT FALSE,
+spectator_area BOOLEAN DEFAULT FALSE,
+FOREIGN KEY (fac_id, reg_id) REFERENCES facility(fac_id, reg_id) ON DELETE CASCADE,
 PRIMARY KEY (fac_id,reg_id)
 );
--- inc/add-region.php
+++ inc/add-region.php
@@ -2,55 +2,55 @@
 
 <?php
 // define variables and set to empty values
-$newRegion = $location = "";
-$regionErr = $locationErr = "";
-$regionFlag = $locationFlag = FALSE;
-$sucess = 0;
+	$newRegion = $location = "";
+	$regionErr = $locationErr = "";
+	$regionFlag = $locationFlag = FALSE;
+	$sucess = 0;
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-	if(isset($_POST["create"])) {
-		if (empty($_POST["newregion"])) {
-			$regionErr = "Region name is required";
-			$regionFlag = $locationFlag = FALSE;
-		} else {
-			$newRegion= $_POST["newregion"];
-			$regionErr = "";
-			$regionFlag = TRUE;
+	if ($_SERVER["REQUEST_METHOD"] == "POST") {
+		if(isset($_POST["create"])) {
+			if (empty($_POST["newregion"])) {
+				$regionErr = "Region name is required";
+				$regionFlag = $locationFlag = FALSE;
+			} else {
+				$newRegion= $_POST["newregion"];
+				$regionErr = "";
+				$regionFlag = TRUE;
+			}
+
+			if (empty($_POST["location"])) {
+				$locationErr = "Region location is required";
+				$regionFlag = $locationFlag = FALSE;
+			} else {
+				$location = $_POST["location"];
+				$locationErr = "";
+				$locationFlag = TRUE;
+			}
+
+		
+			if($regionFlag && $locationFlag) {
+				$root = realpath($_SERVER["DOCUMENT_ROOT"]);
+				include $root.'/cs2102/inc/db-conn.php';
+				$conn = setup_db();
+
+				$query = "SELECT max(reg_id) FROM region;";
+				$result = mysql_query($query);
+				$rows = mysql_fetch_row($result);
+
+				$reg_id = $rows[0]+1;
+
+				$insertQuery = "INSERT INTO region(reg_id, name, location) 
+					VALUES(".$reg_id.", '".mysql_real_escape_string($newRegion)."', '".mysql_real_escape_string($location)."');";
+				$sucess = mysql_query($insertQuery);
+
+				close_db($conn);
+			}
 		}
 
-		if (empty($_POST["location"])) {
-			$locationErr = "Region location is required";
-			$regionFlag = $locationFlag = FALSE;
-		} else {
-			$location = $_POST["location"];
-			$locationErr = "";
-			$locationFlag = TRUE;
-		}
-
-	
-		if($regionFlag && $locationFlag) {
-			$root = realpath($_SERVER["DOCUMENT_ROOT"]);
-			include $root.'/cs2102/inc/db-conn.php';
-			$conn = setup_db();
-
-			$query = "SELECT max(reg_id) FROM region;";
-			$result = mysql_query($query);
-			$rows = mysql_fetch_row($result);
-
-			$reg_id = $rows[0]+1;
-
-			$insertQuery = "INSERT INTO region(reg_id, name, location) 
-				VALUES(".$reg_id.", '".mysql_real_escape_string($newRegion)."', '".mysql_real_escape_string($location)."');";
-			$sucess = mysql_query($insertQuery);
-
-			close_db($conn);
+		if(isset($_POST["back"])){
+			header('Location: /cs2102/inc/admin-panel.php');
 		}
 	}
-
-	if(isset($_POST["back"])){
-		header('Location: /cs2102/inc/admin-panel.php');
-	}
-}
 
 ?>
 
@@ -68,9 +68,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
 
 <?php
-if($sucess) {
-	echo "New Region ".$newRegion." has been added";
-}
+	if($sucess) {
+		echo "New Region ".$newRegion." has been added";
+	}
 ?>
 
 
