70a01034c0bacfa483f81052b1bb0a1f9fdedc55
Anirup <asg.wolfblood@gmail.com>
2013-11-18 06:32:00 +0800

    Changes after CSS

--- inc/confirm-booking.php
+++ inc/confirm-booking.php
@@ -29,22 +29,23 @@
 
 		if($go == 0)
 		{
-			$query1 = "SELECT MAX(book_id) from booking";
-			$max = mysql_query($query1);
-			$rowmax = mysql_fetch_array($max);
-			$val = $rowmax[0] +1;	
-
-			$query = "SELECT count(*) FROM booking WHERE fac_id = ".$_GET['bookfac']." AND start = '".$dtstart."' AND end = '".$dtend."';";
+			$query = "SELECT count(*) FROM booking WHERE fac_id = ".$fac." AND start = '".$dtstart."' AND end = '".$dtend."';";
 			$result = mysql_query($query);
 			$countExist = mysql_fetch_array($result);
 			$countExist = $countExist[0];
 
-		if($countExist == 0) {
-			$query_insert = "INSERT INTO booking (`book_id`,`fac_id`,`reg_id`,`user_id`,`start`,`end`) VALUES(".$val.",".$fac.",".$reg.",'".$user."','".$dtstart."','".$dtend."')";
-			//mysql_query($query_insert);		
-			echo '<script>console.log("Blah"); </script>	';
-		}
-			//echo '<script>window.location.href = "/cs2102/inc/user-bookings.php"; </script>	';	
+			if($countExist == 0)
+			{
+				$query1 = "SELECT MAX(book_id) from booking";
+				$max = mysql_query($query1);
+				$rowmax = mysql_fetch_array($max);
+				$val = $rowmax[0] +1;	
+				
+				$query_insert = "INSERT INTO booking (`book_id`,`fac_id`,`reg_id`,`user_id`,`start`,`end`) VALUES(".$val.",".$_GET['bookfac'].",".$_GET['bookreg'].",'".$_GET['bookuser']."','".$dtstart."','".$dtend."')";
+				mysql_query($query_insert);		
+				echo '<script>console.log("Blah"); </script>	';
+				echo '<script>window.location.href = "/cs2102/inc/user-bookings.php"; </script>	';
+			}	
 		}
 
 		else 
